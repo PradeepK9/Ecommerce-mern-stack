e78689948b2747103af85cb9b2f1bb959712b833
Pradeep Jaiswal <pradeep88318@mastek.com>
2022-10-21 15:38:49 +0530

    added product routes

--- backend/app.js
+++ backend/app.js
@@ -7,8 +7,6 @@ app.use(express.json());
 // Routes import
 const product = require('./routes/productRoutes');
 
-console.log("app...........")
 app.use('/api/v1', product);
-
 
 module.exports = app;
