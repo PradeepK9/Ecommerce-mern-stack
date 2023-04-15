7184e48fa6a90a210202df71370ac5a8e784bd40
Pradeep Jaiswal <pradeep88318@mastek.com>
2023-04-15 15:00:02 +0530

    added dummy producs on home page

---
+++ frontend/src/component/Product/Product.js
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+import ReactStars from "react-rating-stars-component";
+
+const options = {
+    edit: false,
+    color: "rgba(255,255,255,0.1",
+    activeColor: 'tomato',
+    size: window.innerWidth < 600 ? 15 : 20,
+    value: 2.5,
+    isHalf: true
+}
+
+const Product = ({ product }) => {
+    return (
+        <Link className="productCard" to={product.id}>
+            <img src={product.images[0].url} alt={product.name} />
+            <p>{product.name}</p>
+            <div>
+                <ReactStars {...options} /> <span>446 Reviews</span>
+            </div>
+            <span>{product.price}</span>
+        </Link>
+    )
+}
+
+export default Product;
